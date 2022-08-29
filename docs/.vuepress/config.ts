b26841d0b177a6f637ceac181227347df7e2617b
Tackoil <santoin351@outlook.com>
2022-08-29 17:57:20 +0800

    feat: Add friend link

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ module.exports = {
             { name: '首页', path: "/", icon: "icon-home"},
             // { name: '分类', path: "/categories", icon: "icon-appstore"},
             { name: '关于', path: "/about", icon: "icon-user"},
-            // { name: '友链', path: "/links", icon: "icon-link"},
+            { name: '友链', path: "/links", icon: "icon-link"},
         ],
         subtitle: "We're here."
     }),
